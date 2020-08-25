180faec0c664f49b9c072e5ccd3bc18cbc2a0801
Zachary Nelson <nelsonz2013@hotmail.com>
2020-08-24 20:44:11 -0500

    Proxy updates

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ app.use(express.static('public'));
 
 app.use('/rrmodule', createProxyMiddleware({ target: 'http://ec2-54-172-55-163.compute-1.amazonaws.com', changeOrigin: true }));
 app.use('/qa', createProxyMiddleware({ target: 'http://ec2-18-224-182-50.us-east-2.compute.amazonaws.com', changeOrigin: true }));
+app.use('/ov', createProxyMiddleware({ target: 'http://ec2-35-165-217-158.us-west-2.compute.amazonaws.com', changeOrigin: true }));
+app.use('/rp', createProxyMiddleware({ target: 'http://ec2-3-12-148-239.us-east-2.compute.amazonaws.com', changeOrigin: true }));
 // app.use('/rrmodule', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
 
 app.listen(port, () => {
